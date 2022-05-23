e6faeb14d90f491b9fb018c365d2fabbe32284d3
Stefan Garlonta <stefan.garlonta@gmail.com>
2022-05-23 18:34:18 +0200

    Update frontend user model to include more account info

--- src/main/webapp/app/entities/user/user.model.ts
+++ src/main/webapp/app/entities/user/user.model.ts
@@ -1,10 +1,12 @@
 export interface IUser {
   id?: string;
   login?: string;
+  firstName?: string;
+  lastName?: string;
 }
 
 export class User implements IUser {
-  constructor(public id: string, public login: string) {}
+  constructor(public id: string, public login: string, public firstName: string, public lastName: string) {}
 }
 
 export function getUserIdentifier(user: IUser): string | undefined {
