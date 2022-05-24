538afe0618b00d33b99758036b74abb6d584666a
Stefan Garlonta <stefan.garlonta@gmail.com>
2022-05-24 17:06:26 +0200

    Minimalize viewable project details

--- src/main/webapp/app/entities/project/detail/project-detail.component.ts
+++ src/main/webapp/app/entities/project/detail/project-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { IProject } from '../project.model';
+import { AccountService } from 'app/core/auth/account.service';
+import { Account } from 'app/core/auth/account.model';
 
 @Component({
   selector: 'jhi-project-detail',
@@ -9,13 +11,15 @@ import { IProject } from '../project.model';
 })
 export class ProjectDetailComponent implements OnInit {
   project: IProject | null = null;
+  account: Account | null = null;
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  constructor(protected activatedRoute: ActivatedRoute, private accountService: AccountService) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ project }) => {
       this.project = project;
     });
+    this.accountService.identity().subscribe(account => (this.account = account));
   }
 
   previousState(): void {
