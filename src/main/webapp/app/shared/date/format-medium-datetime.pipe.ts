7bb96674b2714cb8c2a3d82cec910906a10a9a35
Stefan Garlonta <stefan.garlonta@gmail.com>
2022-05-24 18:37:47 +0200

    Change date formatting

--- src/main/webapp/app/shared/date/format-medium-datetime.pipe.ts
+++ src/main/webapp/app/shared/date/format-medium-datetime.pipe.ts
@@ -7,6 +7,6 @@ import dayjs from 'dayjs/esm';
 })
 export class FormatMediumDatetimePipe implements PipeTransform {
   transform(day: dayjs.Dayjs | null | undefined): string {
-    return day ? day.format('D MMM YYYY HH:mm:ss') : '';
+    return day ? day.format('D. MMM YYYY') : '';
   }
 }
