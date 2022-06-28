d8fd0b09e0365d6093f5e7f1c353baa40afb7e2a
Stefan Garlonta <stefan.garlonta@gmail.com>
2022-06-28 12:51:49 +0200

    Add custom Footer

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'jhi-footer',
   templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {}
